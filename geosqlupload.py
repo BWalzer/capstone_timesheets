8a55331e68c45108899787dd777e678ca13415f1
Anne Marie <anne.marie.ou@utexas.edu>
2018-02-27 15:57:27 -0800

    updated for log table

--- geosqlupload.py
+++ geosqlupload.py
@@ -7,39 +7,32 @@ import multiprocessing
 import os
 import json
 import requests
+import time
+import datetime
 
-def main():
-    auth_token = os.environ['CAPSTONE_TOKEN']
+auth_token = os.environ['CAPSTONE_TOKEN']
+headers = {'Authorization': auth_token}
 
-    headers = {'Authorization': auth_token}
-
+def get_response(page):
     date='2000-08-01T12:00:00-06:00'
-    page=1
-    geo_url='https://rest.tsheets.com/api/v1/geolocations?modified_since={}&page={}'.format(date, page)
-    geo_response=requests.get(geo_url, headers=headers)
+    geo_url='https://rest.tsheets.com/api/v1/geolocations'
+    params = {'modified_since': date, 'page': page}
+    geo_response=requests.get(geo_url, headers=headers, params=params)
     geo=json.loads(geo_response.text)
-    df=pd.DataFrame(geo['results']['geolocations'],index=None).T
+    return geo, geo_response
 
-    while geo['more']==True:
-        page+=1
-        geo_url='https://rest.tsheets.com/api/v1/geolocations?modified_since={}&page={}'.format(date, page)
-        geo_response=requests.get(geo_url, headers=headers)
-        df=pd.DataFrame(geo['results']['geolocations'],index=None).T
-        df=pd.concat([df,df],axis=0)
-        print(page)
-
+def uploaddf_tosql(df, logentry):
     db_name = os.environ['CAPSTONE_DB_NAME']
     host = os.environ['CAPSTONE_DB_HOST']
     username = os.environ['CAPSTONE_DB_USERNAME']
     password = os.environ['CAPSTONE_DB_PASSWORD']
 
     conn = psycopg2.connect(database=db_name, user=username, host=host, password=password)
-
     cursor = conn.cursor()
-
 
     template = ', '.join(['%s'] * len(df.columns))
 
+    #table already created with constraints
     query = '''INSERT INTO geo
        (accuracy, altitude, created, device_identifier, heading,
        geo_id, latitude, longitude, source, speed, employee_id)
@@ -48,7 +41,23 @@ def main():
     for index, row in df.iterrows():
         cursor.execute(query=query, vars=row)
 
+    querylog = '''INSERT INTO geolog
+               (page, response, uploaded)
+               VALUES ({})'''.format(template)
+
+
+    cursor.execute(query=querylog, vars=logentry)
+
     conn.commit()
 
-if __name__ == '__main__':
-    main()
+def main():
+    page=1
+    geo, georesponse=get_response(page)
+    df=pd.DataFrame(geo['results']['geolocations'],index=None).T
+
+    while geo['more']==True:
+        page+=1
+        geo, georesponse=get_response(page)
+        df=pd.DataFrame(geo['results']['geolocations'],index=None).T
+        logentry=[page, int(str(georesponse)[11:14]), datetime.datetime.now()]
+        uploaddf_tosql(df, logentry)
