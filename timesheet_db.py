33ea7284f8900f9e271608fc169263872c09e177
Yichen Qiu <yichen.qiu@gmail.com>
2018-02-28 13:23:11 -0800

    final timesheet_db

--- timesheet_db.py
+++ timesheet_db.py
@@ -18,20 +18,26 @@ def clean_to_df(data):
     return timesheet_df
 
 def request_data_API(auth_token,headers):
-    page=1
-    while response.json()['more']:
+    page=2
+    url = 'https://est.tsheets.com/api/v1/timesheets?start_date=2017-01-01&page=1'
+    print('requesting page 1')
+    data = requests.get(url, headers=headers)
+    timesheet=clean_to_df(data)
+    while True:
         print('requesting page {}'.format(page))
         url = 'https://est.tsheets.com/api/v1/timesheets?start_date=2017-01-01&page={}'.format(page)
         data = requests.get(url, headers=headers)
         timesheet_df=clean_to_df(data)
         timesheet = pd.concat([timesheet, timesheet_df])
         page += 1
+        if data.json()['more']==False:
+            break
     return timesheet
 
-def insert_to_DB(db_name, username, host, password,timesheet):
+def insert_to_DB(db_name, username, host, password,timesheets):
     conn = psycopg2.connect(database=db_name, user=username, host=host, password=password)
     cursor = conn.cursor()
-    template = ', '.join(['%s'] * len(timesheet.columns))
+    template = ', '.join(['%s'] * len(timesheets.columns))
 
     query = '''INSERT INTO timesheets
            (date,duration,end_time,start_time,
@@ -39,7 +45,7 @@ def insert_to_DB(db_name, username, host, password,timesheet):
            on_the_clock,type,timezone,tz_str,user_id,customfields,attached_files)
                VALUES ({})'''.format(template)
     duprow_count=0
-    for _, timesheet in timesheet.iterrows():
+    for _, timesheet in timesheets.iterrows():
         try:
             cursor.execute(query=query, vars=timesheet)
         except psycopg2.IntegrityError:
@@ -47,8 +53,8 @@ def insert_to_DB(db_name, username, host, password,timesheet):
             duprow_count+=1
             conn.rollback()
         conn.commit()
-
     conn.close()
+    print (duprow_count)
 
 if __name__=="__main__":
     db_name = os.environ['CAPSTONE_DB_NAME']
