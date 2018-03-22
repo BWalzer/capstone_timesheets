ecbe7d655957ce5d66d0b533cd63d056ac313532
Anne Marie <anne.marie.ou@utexas.edu>
2018-03-22 16:52:11 -0700

    added Yichen code

--- dashboardscraper.py
+++ dashboardscraper.py
@@ -3,8 +3,38 @@ import os
 from selenium import webdriver
 import selenium
 import time
+from selenium.webdriver import Firefox,Chrome
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+
+def login(browser,url,email,password):
+    browser.get(url)
+    time.sleep(4)
+    emailElem = browser.find_element_by_css_selector('input#username')
+    emailElem.send_keys(email)
+    time.sleep(1)
+    pwElem = browser.find_element_by_css_selector('input#password')
+    pwElem.send_keys(password)
+    time.sleep(1)
+    signin_button=browser.find_element_by_css_selector("button.flat.primary.action")
+    signin_button.click()
+    time.sleep(4)
+
+def navigate_to_timesheet(browser,url,email,password):
+    login(browser,url,email,password)
+    report_more=browser.find_element_by_xpath("//div[@id='TT_reports_shortcut']\
+                                          /span[@class='flyout_text more_arrow_label']")
+    report_more.click()
+    time.sleep(1)
+    logging_auditing=browser.find_element_by_xpath("//div[@id='wwTT_reports']/div[@id='report_menu_container']/div/div[@id='section_header_logs']")
+    logging_auditing.click()
+    time.sleep(1)
+    timesheet_log=browser.find_element_by_xpath("//div[@id='wwTT_reports']/div[@id='report_menu_container']/div/div[@id='section_links_logs']\
+        /ul/li[@id='main_menu_time_log']")
+    timesheet_log.click()
+
+
 
 
 
@@ -34,20 +64,18 @@ def click_download():
     dl_button=browser.find_element_by_css_selector('button#addon_reports_builder_formsubmit_download_sql')
     dl_button.click()
 
-
-def dl_all_files():
+def dl_files():
     #currently 106 timesheet logs
     optiontags=find_all_date_files
     for i in range(len(optiontags)):
         origdate=find_origdate(i, optiontags)
         lookupkey=make_lookup_key(origdate)
         select_file(lookupkey)
-        click_download
-
+        click_download()
+        time.sleep(5)
 
 def uploadfile_tobucket(filename):
     #need to make directory and organize the files in one place
-
     bucket_name='capstone-timesheet-data'
     foldername='timesheetdata'
 
@@ -56,3 +84,14 @@ def uploadfile_tobucket(filename):
     aws_base_command='aws s3 sync {}/{}'.format(foldername,filename)
 
     os.system(aws_base_command+" {}".format(bucketloc))
+
+if __name__ == '__main__':
+    #log in and select to get the editlog box open
+    url='https://capstonesolutions.tsheets.com/page/login'
+    email=os.environ['CAPSTONE_EMAIL']
+    password=os.environ ['CAPSTONE_PASS']
+    browser = Firefox()
+    navigate_to_timesheet(browser,url,email,password)
+
+    #once log box is open
+    dl_files()
