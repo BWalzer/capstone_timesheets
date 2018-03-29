600ac516c794ddefb43f796d3ad40218902e347c
Ben Walzer <bwalzer@me.com>
2018-03-29 14:52:03 -0700

    adding logging for stderr and stdout

--- database_management/update_database.sh
+++ database_management/update_database.sh
@@ -1,14 +1,14 @@
+cd ~/Documents/capstone_timesheets/database_management
+# python request_new_timesheet.py 1>../logs/timesheets.log 2>>../logs/timesheets.log
+python request_new_customfield_items.py 1>../logs/customfield_items.log  2>>../logs/customfield_items.log
+# python request_new_geo.py 1>../logs/geo.log 2>>../logs/geo.log
+python request_new_customfields.py 1>../logs/customfields.log 2>>../logs/customfields.log
+python request_new_jobcodes.py 1>../logs/jobcodes.log 2>>../logs/jobcodes.log
+python request_new_employees.py 1>../logs/employees.log 2>>../logs/employees.log
 
-# python request_new_timesheet.py > ../logs/timesheets.log
-python request_new_customfield_items.py > ../logs/customfield_items.log
-# python request_new_geo.py > ../logs/geo.log
-python request_new_customfields.py > ../logs/customfields.log
-python request_new_jobcodes.py > ../logs/jobcodes.log
-python request_new_employees.py > ../logs/employees.log
-
-python upload_employees.py >> ../logs/employees.log
-python upload_jobcodes.py >> ../logs/jobcodes.log
-python upload_customfields.py >> ../logs/customfields.log
-# python upload_geo.py >> ../logs/geo.log
-python upload_customfield_items.py >> ../logs/customfield_items.py
-# python upload_timesheet.py >> ../logs/timesheets.log
+python upload_employees.py 1>>../logs/employees.log 2>>../logs/employees.log
+python upload_jobcodes.py 1>>../logs/jobcodes.log 2>>../logs/jobcodes.log
+python upload_customfields.py 1>> ../logs/customfields.log 2>> ../logs/customfields.log
+# python upload_geo.py 1>>../logs/geo.log 2>>../logs/geo.log
+python upload_customfield_items.py 1>>../logs/customfield_items.log 2>>../logs/customfield_items.log
+# python upload_timesheet.py 1>>../logs/timesheets.log 2>>../logs/timesheets.log
