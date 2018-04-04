e108d425c5d1763c9f019e7ea51b6559750f8241
Ben Walzer <bwalzer@me.com>
2018-04-04 11:18:52 -0700

    fixing import issue

--- database_management/request_new_timesheet_logs.py
+++ database_management/request_new_timesheet_logs.py
@@ -147,7 +147,7 @@ def upload_to_s3(content, bucket_name, s3_client, path):
 def get_file_paths():
     #get's all file names in the downloads folder
     path='/home/ubuntu/Downloads'
-    allfiles = [f for f in os.listdir(path) if os.isfile(join(path, f))]
+    allfiles = [f for f in os.listdir(path) if os.path.isfile(join(path, f))]
     return allfiles
 
 if __name__ == '__main__':
