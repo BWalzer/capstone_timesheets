0fd43664b904d142769988fe98b55ab9b4522cc8
Anne Marie <anne.marie.ou@utexas.edu>
2018-04-04 11:24:16 -0700

    path changed to be absolute for seeing files in DL folder

--- database_management/request_new_timesheet_logs.py
+++ database_management/request_new_timesheet_logs.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+from os.path import isfile, join
 from selenium import webdriver
 import selenium
 import time
@@ -146,7 +147,7 @@ def upload_to_s3(content, bucket_name, s3_client, path):
 
 def get_file_paths():
     #get's all file names in the downloads folder
-    path='../Downloads'
+    path='/home/ubuntu/Downloads'
     allfiles = [f for f in os.listdir(path) if isfile(join(path, f))]
     return allfiles
 
